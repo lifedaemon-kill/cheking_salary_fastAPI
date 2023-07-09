739ddf3ab6ac3eba424b824e793d651a59acec82
lifedaemon-kill <noname0263@gmail.com>
2023-07-10 02:07:22 +0900

    restructed

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="Trading App"
+    title="Cheking salary"
 )
 
-
-fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
+employees = [
+    {"id": 1, "name": "Товарищ Р.",             "salary_increase_date": "11.07.2023"},
+    {"id": 2, "name": "Г М", "salary_increase_date": "13.08.2023"},
+    {"id": 3, "name": "Линус Торвальдс",        "salary_increase_date": "01.01.2024"},
 ]
 
-
-@app.get("/users/{user_id}")
+@app.get("/empl/{empl_id}")
 def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
+    return [user for user in employees if user.get("id") == user_id]
 
 
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
-]
+@app.get("/date_inc_salary")
+def get_salary_increase_date(user_id, login=0, password=0):
+    return [empl for empl in employees if empl.get("id") == user_id]
 
-
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
-
-
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
